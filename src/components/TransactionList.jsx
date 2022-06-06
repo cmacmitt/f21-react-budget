059e7462c10e6564756e6aba35c363615f67e955
Chris MacDonald <chris.macdonald@mitt.ca>
2022-06-06 10:54:46 -0500

    fix transaction list layout styles

--- src/components/TransactionList.jsx
+++ src/components/TransactionList.jsx
@@ -5,7 +5,7 @@ const TransactionList = ({ type }) => {
   // temporary hack until we fix the styles
   if (type === "incomes") type = "income";
   return (
-    <div className={styles.type}>
+    <div className={styles[type]}>
       <h2 className={`${styles[type]}__title`}>{type.toUpperCase()}</h2>
       <div className={`${styles[type]}__list`}>
         <Transaction type={type} />
